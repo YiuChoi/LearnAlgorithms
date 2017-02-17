a1a8ec62ae0f3d9901809b2a1b8a0b442c6f45ed
蔡小木 <whucaiyao@gmail.com>
2017-02-17 17:27:31 +0800

    12

---
+++ Graph/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 蔡小木 on 2017/2/17 0017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        new Graph().dfs();
+    }
+}
---
+++ Graph/src/Vertex.java
@@ -0,0 +1,12 @@
+/**
+ * Created by 蔡小木 on 2017/2/17 0017.
+ */
+public class Vertex {
+    public char label;
+    public boolean visited;
+
+    public Vertex(char label) {
+        this.label = label;
+        this.visited = false;
+    }
+}
